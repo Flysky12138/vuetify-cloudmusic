1c810c98f1c0c0f512d14afa1b6471b2a6128d02
Flysky12138 <2797299614@qq.com>
2021-01-24 22:09:32 +0800

    0124

--- src/components/AppBar/BtLink.vue
+++ src/components/AppBar/BtLink.vue
@@ -1,26 +1,58 @@
 <template>
   <v-sheet>
-    <v-btn
-      v-for="name in names"
-      :key="name.id"
-      text
-      rounded
-      class="mr-6 text-subtitle-1 font-weight-bold"
-      width="80"
+    <v-menu
+      v-for="item in bt"
+      :key="item.id"
+      rounded="lg"
+      transition="slide-y-transition"
     >
-      {{ name }}
-      <v-icon class="mt-1" color="grey lighten-1" small>
-        {{ icons[0] }}
-      </v-icon>
-    </v-btn>
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn
+          v-bind="attrs"
+          v-on="on"
+          text
+          rounded
+          class="mr-6 text-subtitle-1 font-weight-bold"
+          width="80"
+        >
+          {{ item.name }}
+        </v-btn>
+      </template>
+      <v-list v-if="item.lists != undefind">
+        <v-list-item
+          v-for="i in item.lists"
+          :key="i.id"
+          class="px-0 justify-center"
+        >
+          <v-list-item-action class="ma-0">
+            <v-btn text rounded class="text-subtitle-2 font-weight-bold">
+              {{ i }}
+            </v-btn>
+          </v-list-item-action>
+        </v-list-item>
+      </v-list>
+    </v-menu>
   </v-sheet>
 </template>
 
 <script>
 export default {
   data: () => ({
-    names: ["首页", "歌单", "个性", "关于"],
-    icons: ["mdi-chevron-down", "mdi-chevron-up"],
+    bt: [
+      {
+        name: "首页",
+      },
+      {
+        name: "歌单",
+        lists: ["推荐", "官方", "精品", "华语", "流行", "欧美"],
+      },
+      {
+        name: "个性",
+      },
+      {
+        name: "关于",
+      },
+    ],
   }),
 };
 </script>
--- src/components/MainView.vue
+++ src/components/MainView.vue
@@ -1,6 +1,6 @@
 <template>
   <v-container>
-    <v-sheet min-height="88vh" rounded="xl">
+    <v-sheet min-height="88vh" rounded="lg">
       <slot />
     </v-sheet>
   </v-container>
