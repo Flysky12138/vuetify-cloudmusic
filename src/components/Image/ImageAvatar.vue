b60bda4b4dba10dbd92aaf3f4ba4a477194080b5
Flysky12138 <2797299614@qq.com>
2021-03-15 12:16:11 +0800

    0315

--- src/components/Image/ImageAvatar.vue
+++ src/components/Image/ImageAvatar.vue
@@ -1,24 +1,20 @@
 <template>
-  <div class="d-flex">
-    <v-avatar size="38">
-      <v-btn icon :to="{ path: '/user', query: { uid: uid } }">
-        <v-img :src="src" max-width="38">
-          <!-- 图片加载前显示 -->
-          <template v-slot:placeholder>
-            <v-row class="fill-height ma-0" align="center" justify="center">
-              <v-progress-circular
-                indeterminate
-                color="grey lighten-5"
-                size="25"
-              ></v-progress-circular>
-            </v-row>
-          </template>
-        </v-img>
-      </v-btn>
-    </v-avatar>
-    <!-- 默认插槽 -->
-    <slot />
-  </div>
+  <v-avatar size="38">
+    <v-btn icon :to="{ path: '/user', query: { uid: uid } }">
+      <v-img :src="src" max-width="38">
+        <!-- 图片加载前显示 -->
+        <template v-slot:placeholder>
+          <v-row class="fill-height ma-0" align="center" justify="center">
+            <v-progress-circular
+              indeterminate
+              color="grey lighten-5"
+              size="25"
+            ></v-progress-circular>
+          </v-row>
+        </template>
+      </v-img>
+    </v-btn>
+  </v-avatar>
 </template>
 
 <script>
--- src/layout/AppBar/Avatar.vue
+++ src/layout/AppBar/Avatar.vue
@@ -1,17 +1,17 @@
 <template>
-  <image-avatar
-    v-if="islogin"
-    :uid="user.uid"
-    :src="user.avatarUrl"
-    @contextmenu.native.prevent="logout"
-  >
+  <div v-if="islogin" class="d-flex">
+    <image-avatar
+      :uid="user.uid"
+      :src="user.avatarUrl"
+      @contextmenu.native.prevent="logout"
+    ></image-avatar>
     <!-- 等级显示 -->
     <span
       class="mb-n1 align-self-end green--text text-caption font-weight-black font-italic"
     >
       {{ "Lv." + user.level }}
     </span>
-  </image-avatar>
+  </div>
   <v-avatar v-else color="grey lighten-2" size="38">
     <v-btn icon to="/login">
       <v-icon>mdi-account-outline</v-icon>
