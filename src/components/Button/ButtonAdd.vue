c0b0263415290f69ae8a07cda99805e6ae54de31
Flysky12138 <2797299614@qq.com>
2021-08-10 15:38:19 +0800

    0810 - +歌曲收藏显示歌单列表的过渡动画

--- src/components/Button/ButtonAdd.vue
+++ src/components/Button/ButtonAdd.vue
@@ -6,13 +6,22 @@
       </v-btn>
     </template>
     <v-card max-height='400' class='overflow-y-auto scrollbar-hidden' rounded='lg'>
-      <v-list-item v-for='item in playlist' :key='item.id' @click='addSong(item.id)' class='py-2'>
-        <v-img :src='item.coverImgUrl' max-width='60' class='mr-4 rounded-lg'></v-img>
-        <div class='d-flex flex-column justify-space-between py-1' style='height: 60px'>
-          <div class='font-weight-bold text-truncate' style='max-width: 250px'>{{ item.name }}</div>
-          <div class='font-weight-bold text-subtitle-1'>{{ item.trackCount }} 首</div>
-        </div>
-      </v-list-item>
+      <template v-if='showlists'>
+        <v-overlay :value='dialog'>
+          <v-progress-circular indeterminate size='64' width='10'></v-progress-circular>
+        </v-overlay>
+      </template>
+      <template v-else>
+        <v-list>
+          <v-list-item v-for='item in playlist' :key='item.id' @click='addSong(item.id)' class='py-2'>
+            <v-img :src='item.coverImgUrl' max-width='60' class='mr-4 rounded-lg'></v-img>
+            <div class='d-flex flex-column justify-space-between py-1' style='height: 60px'>
+              <div class='font-weight-bold text-truncate' style='max-width: 250px'>{{ item.name }}</div>
+              <div class='font-weight-bold text-subtitle-1'>{{ item.trackCount }} 首</div>
+            </div>
+          </v-list-item>
+        </v-list>
+      </template>
     </v-card>
   </v-dialog>
 </template>
@@ -26,6 +35,7 @@ export default {
   },
   data: () => ({
     dialog: false,
+    showlists: false,
     // 创建的歌单 {coverImgUrl:String, id:Number, name:String, playCount:Number}
     playlist: []
   }),
@@ -44,8 +54,17 @@ export default {
     },
     // 获取用户创建的歌单
     getCreatePlaylist() {
+      this.showlists = true
+      const startTime = new Date()
       this.$http.user.playlist(this.uid).then(res => {
-        this.playlist = this.nolove ? res.create.filter(res => !/^.+喜欢的音乐$/.test(res.name)) : res.create
+        this.playlist = this.nolove ? res.create.filter(res => !/喜欢的音乐$/.test(res.name)) : res.create
+        const delay = new Date() - startTime
+        setTimeout(
+          () => {
+            this.showlists = false
+          },
+          delay < 500 ? 500 : 0
+        )
       })
     }
   }
