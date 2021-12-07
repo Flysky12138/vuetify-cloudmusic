6da7a52891b4cf0d04fc74e60adc8de820c21720
Flysky12138 <2797299614@qq.com>
2021-12-08 00:34:06 +0800

    1208 - fix bug

--- src/views/User/components/UserListenRanking.vue
+++ src/views/User/components/UserListenRanking.vue
@@ -31,7 +31,7 @@
           </template>
           <!-- item.artists插槽 -->
           <template v-slot:item.song.artists='{ item }'>
-            <span v-for='(_item, index) in item.song.artists' :key='_item.id'>
+            <span v-for='(_item, index) in item.song.artists' :key='index'>
               <span v-if='index !== 0'>&nbsp;/&nbsp;</span>
               <button @click='lookArtists(_item.id)'>{{ _item.name }}</button>
             </span>
