00db8dd54ef8baa25114e878ef24a2ed62f69d9c
Flysky12138 <2797299614@qq.com>
2021-12-19 16:57:59 +0800

    1219

--- src/views/About/components/Tools/BtnCloud.vue
+++ src/views/About/components/Tools/BtnCloud.vue
@@ -2,7 +2,7 @@
   <div class='d-flex align-center'>
     <span>云盘：</span>
     <v-btn icon to='/cloud'>
-      <v-icon>mdi-cloud</v-icon>
+      <v-icon color='primary'>mdi-cloud</v-icon>
     </v-btn>
   </div>
 </template>
