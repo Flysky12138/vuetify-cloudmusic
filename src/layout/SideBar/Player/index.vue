82a5dd725ab4932aaccab19f73f32215bff75870
Flysky12138 <x2797299614@gmail.com>
2022-08-01 15:47:04 +0800

    fix(player view): 背景四周出现黑圈

--- src/layout/SideBar/Player/index.vue
+++ src/layout/SideBar/Player/index.vue
@@ -1,31 +1,31 @@
 <template>
-	<v-container @mousewheel='mouseWheel' class='pa-0 overflow-hidden' fluid style='position: relative; height: 100vh; min-width: 900px'>
-		<!-- 背景 -->
-		<v-img :src='music.picUrl' :style='`filter: blur(${blur}px)`' class='bg'></v-img>
-		<!-- 内容 -->
-		<v-card color='transparent' elevation='0' height='100%' style='backdrop-filter: brightness(70%)'>
-			<!-- 左上播放列表按键 -->
-			<player-lists v-on='$listeners' />
-			<!-- 右上关闭页面按键 -->
-			<v-btn @click='$emit("close")' absolute icon large right top>
-				<v-icon large>mdi-chevron-down</v-icon>
-			</v-btn>
-			<v-row align='center' class='ma-0' style='height: 100%'>
-				<!-- 播放 -->
-				<v-col cols='6'>
-					<player-music />
-				</v-col>
-				<!-- 歌词 -->
-				<v-col cols='5' v-if='true'>
-					<player-lyrics />
-				</v-col>
-				<!-- 评论 -->
-				<v-col cols='5' v-else>
-					<player-comment />
-				</v-col>
-			</v-row>
-		</v-card>
-	</v-container>
+  <v-container @mousewheel="mouseWheel" class="pa-0 overflow-hidden" fluid style="position: relative; height: 100vh; min-width: 900px">
+    <!-- 背景 -->
+    <v-img :src="music.picUrl" :style="`filter: blur(${blur}px)`" class="player-bg"></v-img>
+    <!-- 内容 -->
+    <v-card color="transparent" elevation="0" height="100%" style="backdrop-filter: brightness(70%)">
+      <!-- 左上播放列表按键 -->
+      <player-lists v-on="$listeners" />
+      <!-- 右上关闭页面按键 -->
+      <v-btn @click="$emit('close')" absolute icon large right top>
+        <v-icon large>mdi-chevron-down</v-icon>
+      </v-btn>
+      <v-row align="center" class="ma-0" style="height: 100%">
+        <!-- 播放 -->
+        <v-col cols="6">
+          <player-music />
+        </v-col>
+        <!-- 歌词 -->
+        <v-col cols="5" v-if="true">
+          <player-lyrics />
+        </v-col>
+        <!-- 评论 -->
+        <v-col cols="5" v-else>
+          <player-comment />
+        </v-col>
+      </v-row>
+    </v-card>
+  </v-container>
 </template>
 
 <script>
@@ -55,9 +55,10 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.bg {
+.player-bg {
   width: 100%;
   height: 100%;
   position: absolute;
+  transform: scale(1.2);
 }
 </style>
